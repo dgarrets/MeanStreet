1a4b3a83399a8523fd168472b5b8e0c86eedd490
dgarrets <dgarrets@trinity.edu>
2018-01-26 01:07:42 -0600

    First commit

---
+++ MeanStreetPrototype/Assets/Scripts/BurnoutMovement.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BurnoutMovement : MonoBehaviour {
+
+	public float speed = 50;
+	public float topSpeed = 50;
+	public float turnForce = 50;
+	Rigidbody rb;
+
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float inputY = Input.GetAxis("Vertical");
+		Vector3 currentForwardVelocity = Vector3.Project (rb.velocity, transform.forward);
+		rb.AddForce ((transform.forward * inputY * speed)*(1-(currentForwardVelocity.magnitude/topSpeed)));
+
+		float inputX = Input.GetAxis ("Horizontal");
+		rb.AddTorque (transform.up * (inputX * turnForce));
+	}
+}
---
+++ MeanStreetPrototype/Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour {
+
+	public GameObject car;
+	public float distance = 1f;
+
+	// Use this for initialization
+	void Start (){
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.position = car.transform.position + (car.transform.forward * -(3f*distance)) + new Vector3(0,distance,0);
+		transform.rotation = car.transform.rotation;
+	}
+}
